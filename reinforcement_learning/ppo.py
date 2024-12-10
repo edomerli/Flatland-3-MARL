9d40c60f6d3b424d85b913b32c1e2da9094943b5
Edoardo Merli <merliedoardo@gmail.com>
2024-12-10 17:18:32 +0000

    bug fix

--- reinforcement_learning/ppo.py
+++ reinforcement_learning/ppo.py
@@ -143,8 +143,6 @@ class PPO:
 
         for t in reversed(range(self.iterations_timesteps)):
             if t == self.iterations_timesteps - 1:
-                print(self.actor_critic.get_device())
-                print(next_obs.get_device())
                 next_value = self.actor_critic.value(next_obs.unsqueeze(0)) # TODO: potrei usare self.actor_critic.value() per non sprecare computazione con l'actor?
                 next_nonterminal = 1 - next_done
             else:
