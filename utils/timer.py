029e2cac73eb3cc751ee101b879f12f2472a643d
Edoardo Merli <merliedoardo@gmail.com>
2024-12-24 19:08:23 +0000

    Went from computing average time to cumulative time between start and stop calls

--- utils/timer.py
+++ utils/timer.py
@@ -4,7 +4,6 @@ class Timer:
     def __init__(self):
         self.start_time = None
         self.accumulator = 0
-        self.count = 0
 
     def start(self):
         self.start_time = default_timer()
@@ -12,7 +11,6 @@ class Timer:
     def stop(self):
         self.end_time = default_timer()
         self.accumulator += self.end_time - self.start_time
-        self.count += 1
     
-    def avg_elapsed(self):
-        return self.accumulator / self.count
+    def cumulative_elapsed(self):
+        return self.accumulator
