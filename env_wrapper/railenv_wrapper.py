88d221d6f4b9044379715f25f6ff387ec10b32c2
Edoardo Merli <merliedoardo@gmail.com>
2024-12-28 17:38:36 +0000

    documented code and minor late arrival penalty adjustment

--- env_wrapper/railenv_wrapper.py
+++ env_wrapper/railenv_wrapper.py
@@ -14,12 +14,23 @@ from observation.deadlock_checker import DeadlockChecker
 
 class RailEnvWrapper:
     def __init__(self, env: RailEnv):
+        """Class constructor
+
+        Args:
+            env (RailEnv): the flatland environment to wrap
+        """
         self.env = env
 
         self._decision_cells = None
         self.deadlock_checker = DeadlockChecker(env)
 
     def reset(self, **kwargs):
+        """Reset the environment. 
+        Builds the set of decision cells and resets the deadlock checker. Cycles until at least one agent is ready to depart.
+
+        Returns:
+            obs, info: the first observation and info dict from the environment where at least one agent is ready to depart
+        """
         obs, info = self.env.reset(**kwargs)
         self._decision_cells = find_decision_cells(self.env)
         self.deadlock_checker.reset()
@@ -38,23 +49,35 @@ class RailEnvWrapper:
         return obs, info
         
     def step(self, action_dict: Dict[int, RailEnvActions]):
+        """Step the environment with the given actions.
+
+        Overrides the action_required info in the info dict s.t.
+        Before:
+            info["action_required"][agent_id] is True iff
+                1) the agent is in a READY_TO_DEPART state
+                OR
+                2) the agent is on the map (i.e. either MOVING, STOPPED or MALFUNCTION) AND is at a cell entry point
+
+        After:
+            info["action_required"][agent_id] is True iff
+                1) ...same as above...
+                OR 
+                2) ...same as above... AND the agent is on a decision cell (i.e. a cell where the agent can choose its direction!)
+
+        Args:
+            action_dict (Dict[int, RailEnvActions]): the actions to take for each agent
+
+        Returns:
+            obs, reward, done, info: the next observation, reward, done flag and info dict from the environment. Note: only the info dict is modified
+        """
         obs, reward, done, info = self.env.step(action_dict)
 
-        # override action_required info
-        # info["action_required"][agent_id] given from env.step() is True iff:
-            # 1) the agent is in a READY_TO_DEPART state
-            # OR
-            # 2) the agent is on the map (i.e. either MOVING, STOPPED or MALFUNCTION) AND is at a cell entry point
-
-        # we want to override this info s.t. info["action_required"][agent_id] is True iff:
-            # 1) ...same as above...
-            # OR 
-            # 2) ...same as above... AND the agent is on a decision cell (i.e. a cell where the agent can choose its direction!)
+        # update action_required in the info dict
         for agent in self.env.agents:
             if info["action_required"][agent.handle] and (TrainState.MOVING <= agent.state <= TrainState.STOPPED):  # i.e. condition 2) above
                 info["action_required"][agent.handle] = self._on_decision_cell(agent)
         # TODO: action_required is false if the agent is in a deadlock?? Try it!    
-        
+
         return obs, reward, done, info
 
     def _on_decision_cell(self, agent: EnvAgent):
@@ -63,7 +86,26 @@ class RailEnvWrapper:
     def __getattr__(self, name):
         return getattr(self.env, name)
 
-    def custom_reward(self, done, old_info, info):        
+    def custom_reward(self, done, old_info, info):   
+        """Compute the custom reward function.
+        The custom reward function is the sum of the following components:
+            1) a penalty for the agents that did not arrive at their target in time, if the episode is terminated.
+                This is computed as the sum of the penalties for each agent, as computed by the standard environment (env._handle_end_reward),
+                divided by the number of agents and the maximum number of steps in the episode (to normalize)
+            2) 0.1 * (the number of agents that have just departed / the total number of agents)
+            3) 5 * (the number of agents that have just arrived WITH A PENALTY FOR ARRIVING LATE / the total number of agents).
+                The penalty for arriving late is computed as the number of steps that the agent arrived late, normalized by the number of steps in the episode
+            4) -2.5 * (the number of agents that have just entered deadlock / the total number of agents)
+
+        Args:
+            done (dict): the dictionary returned by the env containing the done flag for each agent + the __all__ flag
+            old_info (dict): the info dict from the previous step
+            info (dict): the info dict from the current step
+
+        Returns:
+            float: the custom reward function value
+        """
+
         no_arrival_penalty = 0
         newly_departed_agents = 0
         newly_arrived_agents = 0
@@ -72,11 +114,8 @@ class RailEnvWrapper:
             if old_info["state"][handle] < TrainState.MOVING and info["state"][handle] >= TrainState.MOVING:
                 newly_departed_agents += 1
             if old_info["state"][handle] < TrainState.DONE and info["state"][handle] == TrainState.DONE:
-                newly_arrived_agents += 1 - max(agent.arrival_time - agent.latest_arrival, 0) / (self.env._max_episode_steps - agent.latest_arrival + 1)    # +1 to avoid division by zero
-                # TODO: remove
-                if agent.arrival_time - agent.latest_arrival > 0:
-                    print(f"Agent {handle} arrived {agent.arrival_time - agent.latest_arrival} steps late. Newly arrived agents: {newly_arrived_agents}")
-            if done["__all__"] and agent.state != TrainState.DONE:
+                newly_arrived_agents += 1 - max(agent.arrival_time - agent.latest_arrival, 0) / self.env._max_episode_steps
+            if done["__all__"] and agent.state != TrainState.DONE:  # TODO: se aggiungi fine prima del tempo, be aware of this done["__all__"]
                 no_arrival_penalty += self.env._handle_end_reward(agent)
 
         # count number of True values in self.deadlock_checker.agent_deadlock
@@ -90,19 +129,19 @@ class RailEnvWrapper:
         # no_arrival_penalty is normalized in the same way that the true episodic reward is
         return no_arrival_penalty / N / self.env._max_episode_steps + 0.1 * (newly_departed_agents / N) + 5 * (newly_arrived_agents / N) - 2.5 * (new_deadlocks / N)
        
-    def normalized_reward(self, done, reward):
-        # if self.is_done(done, self.env.get_info_dict()):
-        #     if not done["__all__"]: # TODO: this will always be True if we call normalized_reward at the end of the episode! No matter what!! Instead should use agent.state == TrainState.DONE
-        #         # if not all agents are done, but the assert above passed, it means that some agents are done and some are in deadlock
-        #         # -> we make the episode finish earlier by setting elapsed steps to max_episode and use it to compute the end of episode reward.
-        #         # This way we don't pollute the replay buffer with useless transitions
-        #         self.env._elapsed_steps = self.env._max_episode_steps
-        #         for agent in self.env.agents:
-        #             reward[agent.handle] = self.env._handle_end_reward(agent)
+    def normalized_reward(self, reward):
+        """Computes the true episodic reward as defined by Flatland, i.e. the normalized sum of the rewards of all agents.
 
+        Args:
+            reward (dict): the reward dictionary returned by the environment
+
+        Returns:
+            float: the true episodic reward
+        """
         return 1.0 + sum(reward.values()) / self.env.get_num_agents() / self.env._max_episode_steps   
 
     def is_done(self, done, info):
+        # TODO: docstring
 
         # TODO: remove
         all_done_or_deadlock = True
