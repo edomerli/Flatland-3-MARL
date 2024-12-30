59d964ce95596c051ce03f0310cfc16e9d6d4e3f
Edoardo Merli <merliedoardo@gmail.com>
2024-12-30 09:44:38 +0000

    doc

--- render.py
+++ render.py
@@ -5,6 +5,9 @@ from IPython.display import clear_output, display
 def render_env(env, wait=True):
     """Render the environment using the PILSVG renderer
 
+    NOTE: on WSL2, you have to have installed XLaunch and selected the option "Disable access control" to be able to render the environment.
+    On top of that, you have to run the command "export DISPLAY=:0" on the terminal before running the code.
+
     Args:
         env (RailEnv): the flatland environment to render
         wait (bool, optional): whether to wait for the rendering to be displayed. Defaults to True.
